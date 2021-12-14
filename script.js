ee894572432b90c4f8b0b6dffc0b85a10edc9644
Haksler <95537635+Haksler@users.noreply.github.com>
2021-12-14 02:30:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeslide(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentslide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("polaroid");
+  var dots = document.getElementsByClassName("coursor");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
